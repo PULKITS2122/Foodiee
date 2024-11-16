1fa8226a54eae7e9a7b6dd4efffb7833a83dc6f6
Pulkit Sharma <pulkit.sharma5914@gmail.com>
2024-11-16 15:00:40 +0100

    fixed msg handler

--- frontend-react/src/components/ChatbotComponent.js
+++ frontend-react/src/components/ChatbotComponent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import io from 'socket.io-client';
 import Header from './Header';
 import InputSection from './InputSection';
@@ -9,25 +9,19 @@ const socket = io('http://localhost:3001');
 const ChatbotComponent = () => {
     const [messages, setMessages] = useState([]);
 
-    const greetingMessage = {
-        id: Date.now(),
-        info: "Hey, How can I assist you today? Let's cook something...",
-        type: "left",
-        name: "Foodiee",
-    };
-
     useEffect(() => {
+        const handleBotMessage = (data) => {
+            const newBotMessage = createMessage(data, "left", "Foodiee");
+            setMessages((prevMessages) => [...prevMessages, newBotMessage]);
+        };
+    
         socket.on('bot-message', handleBotMessage);
+    
         return () => {
             socket.off('bot-message', handleBotMessage);
         };
-    }, [handleBotMessage]);
-
-    const handleBotMessage = (data) => {
-        const newBotMessage = createMessage(data, "left", "Foodiee");
-        setMessages((prevMessages) => [...prevMessages, newBotMessage]);
-    };
-
+    }, []);
+    
     const addMessage = (info, type) => {
         const newUserMessage = createMessage(info, type, "User");
         setMessages((prevMessages) => [...prevMessages, newUserMessage]);
